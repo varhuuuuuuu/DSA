7038b25ea0f18061a058b582b9f5db2b8a09fdb1
varun verma <varun.verma5926@gmail.com>
2026-04-30 14:21:13 +0530

    Add files via upload

---
+++ d1q1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[100];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    int pos, x;
+    scanf("%d", &pos);
+    scanf("%d", &x);
+
+    for (int i = n; i >= pos; i--) {
+        arr[i] = arr[i - 1];
+    }
+
+    arr[pos - 1] = x;
+
+    for (int i = 0; i < n + 1; i++) {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
